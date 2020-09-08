5185d7e82b89a54c56e6e30c457d94513462c28c
William Koller <wkoller25@gmail.com>
2020-09-07 22:45:53 -0300

    refactor: ensure LocalSavePurchases throws if CacheStore.insert throws

--- src/data/usecases/save-purchases/local-save-purchases.spec.ts
+++ src/data/usecases/save-purchases/local-save-purchases.spec.ts
@@ -25,6 +25,12 @@ class CacheStoreSpy implements CacheStore {
       throw new Error()
     })
   }
+
+  simulateInsertError (): void {
+    jest.spyOn(CacheStoreSpy.prototype, 'insert').mockImplementationOnce(() => {
+    throw new Error()
+  })
+  }
 }
 
 const mockPurchases = (): Array<SavePurchases.Params> => [{
@@ -80,5 +86,12 @@ describe('LocalSavePurchases', () => {
     expect(cacheStore.insertCallsCount).toBe(1)
     expect(cacheStore.insertKey).toBe('purchases')
     expect(cacheStore.insertValues).toEqual(purchases)
+  })
+
+  test('Should throw if insert throws', () => {
+    const { sut, cacheStore } = makeSut()
+    cacheStore.simulateInsertError()
+    const promise = sut.save(mockPurchases())
+    expect(promise).rejects.toThrow()
   })
 })
