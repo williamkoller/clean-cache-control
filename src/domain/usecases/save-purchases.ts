46a932208a02dd628b0a42e83aa29f52267e85bf
William Koller <wkoller25@gmail.com>
2020-09-08 00:52:52 -0300

    refactor: add LoadPurchases usecase

--- src/domain/usecases/save-purchases.ts
+++ src/domain/usecases/save-purchases.ts
@@ -1,11 +1,9 @@
+import { PurchaseModel } from '@/domain/models'
+
 export interface SavePurchases {
   save: (purchases: SavePurchases.Params[]) => Promise<void>
 }
 
 export module SavePurchases {
-  export type Params = {
-    id: string
-    date: Date
-    value: number
-  }
+  export type Params = PurchaseModel
 }
