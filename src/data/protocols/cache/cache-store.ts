78fa79c014663c785019b2358344e2ae085bc8ea
William Koller <wkoller25@gmail.com>
2020-09-07 21:53:07 -0300

    feat: ensure LocalSavePurchases insert correct data on Cache

--- src/data/protocols/cache/cache-store.ts
+++ src/data/protocols/cache/cache-store.ts
@@ -1,4 +1,4 @@
 export interface CacheStore {
   delete: (key: string) => void
-  insert: (ley: string) => void
+  insert: (ley: string, value: any) => void
 }
--- src/domain/usecases/save-purchases.ts
+++ src/domain/usecases/save-purchases.ts
@@ -2,7 +2,7 @@ export interface SavePurchases {
   save: (purchases: SavePurchases.Params[]) => Promise<void>
 }
 
-namespace SavePurchases {
+export module SavePurchases {
   export type Params = {
     id: string
     date: Date
