3d542f66dad9c9995d7ddcb817a402ea7d031839
William Koller <wkoller25@gmail.com>
2020-09-08 02:00:11 -0300

    feat: add scripts jest

--- jest.config.js
+++ jest.config.js
@@ -7,7 +7,7 @@ module.exports = {
   collectCoverageFrom: [
     '<rootDir>/src/**/*.ts',
     '!<rootDir>/src/main/**',
-    '!<rootDir>/src/**/*-protocols.ts',
+    '!<rootDir>/src/**/index.ts',
     '!**/protocols/**',
     '!**/test/**'
   ],
