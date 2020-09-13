73830571135fbbe46ad5dc8a04f854d58c231603
William Koller <wkoller25@gmail.com>
2020-09-13 01:20:36 -0300

    feat: add simulateFetchError

--- src/data/tests/mock-cache.ts
+++ src/data/tests/mock-cache.ts
@@ -9,36 +9,43 @@ export class CacheStoreSpy implements CacheStore {
   insertValues: Array<SavePurchases.Params> = []
 
 
-  fetch (key: string): void {
+  fetch(key: string): void {
     this.actions.push(CacheStoreSpy.Action.fetch)
     this.fetchKey = key
   }
-  delete (key: string): void {
+  delete(key: string): void {
     this.actions.push(CacheStoreSpy.Action.delete)
     this.deleteKey = key
   }
 
-  insert (key: string, value: any): void {
+  insert(key: string, value: any): void {
     this.actions.push(CacheStoreSpy.Action.insert)
     this.insertKey = key
     this.insertValues = value
   }
 
-  replace (key: string, value: any): void {
+  replace(key: string, value: any): void {
     this.delete(key)
     this.insert(key, value)
   }
 
-  simulateDeleteError (): void {
+  simulateDeleteError(): void {
     jest.spyOn(CacheStoreSpy.prototype, 'delete').mockImplementationOnce(() => {
       this.actions.push(CacheStoreSpy.Action.delete)
       throw new Error()
     })
   }
 
-  simulateInsertError (): void {
+  simulateInsertError(): void {
     jest.spyOn(CacheStoreSpy.prototype, 'insert').mockImplementationOnce(() => {
       this.actions.push(CacheStoreSpy.Action.insert)
+      throw new Error()
+    })
+  }
+
+  simulateFetchError(): void {
+    jest.spyOn(CacheStoreSpy.prototype, 'fetch').mockImplementationOnce(() => {
+      this.actions.push(CacheStoreSpy.Action.fetch)
       throw new Error()
     })
   }
