ba86227e6ed173457b6f71c55d15d8a99cff81d1
William Koller <wkoller25@gmail.com>
2020-09-29 00:46:50 -0300

    refactor: remove constructor disuse

--- src/data/protocols/cache/cache-policy.ts
+++ src/data/protocols/cache/cache-policy.ts
@@ -1,7 +1,5 @@
 export class CachePolicy {
   private static maxAgeInDays = 3
-  private constructor(){}
-
 
   static validate (timestamp: Date, date: Date): boolean {
     const maxAge = new Date(timestamp)
