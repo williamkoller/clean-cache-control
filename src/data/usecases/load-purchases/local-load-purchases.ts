a281459366a32a29032e6807c59bc1829fe21691
William Koller <wkoller25@gmail.com>
2020-09-28 21:59:39 -0300

    feat: ensure LocalLoadPurchases returns empty list on load failure

--- src/data/usecases/load-purchases/local-load-purchases.ts
+++ src/data/usecases/load-purchases/local-load-purchases.ts
@@ -1,22 +1,28 @@
 import { CacheStore } from '@/data/protocols/cache'
-import { SavePurchases } from '@/domain/usecases'
+import { SavePurchases, LoadPurchases } from '@/domain/usecases'
 
-export class LocalLoadPurchases implements SavePurchases {
+export class LocalLoadPurchases implements SavePurchases, LoadPurchases {
   private readonly key = 'purchases'
 
-  constructor (
+  constructor(
     private readonly cacheStore: CacheStore,
     private readonly timestamp: Date
   ) { }
 
-  async save (purchases: Array<SavePurchases.Params>): Promise<void> {
+  async save(purchases: Array<SavePurchases.Params>): Promise<void> {
     this.cacheStore.replace(this.key, {
       timestamp: this.timestamp,
       value: purchases
     })
   }
 
-  async loadAll (): Promise<void> {
-    this.cacheStore.fetch(this.key)
+  async loadAll(): Promise<Array<LoadPurchases.Result>> {
+    try {
+      this.cacheStore.fetch(this.key)
+      return []
+    } catch (error) {
+      this.cacheStore.delete(this.key)
+      return []
+    }
   }
 }
